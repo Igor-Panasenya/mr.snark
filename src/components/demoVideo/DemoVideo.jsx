e418f43f7477c091d836baf8d7e6338f5ec86838
Игорь Михайлович Панасеня <panasenya1995@mail.ru>
2022-10-04 18:03:38 +0300

    Initialize project using Create React App

--- src/components/demoVideo/DemoVideo.jsx
+++ src/components/demoVideo/DemoVideo.jsx
@@ -14,7 +14,7 @@ const DemoVideo = () => {
                 <div className="demoVideo-content">
 
                     <div className="demoVideo-box">
-                        <video width="100%" height="100%" controls="">
+                        <video width="100%" height="100%" controls>
                             <source src={video} type="video/mp4"/>
                         </video>
                     </div>
